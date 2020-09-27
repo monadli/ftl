866bdd9814d5eed99cee5afa79381731f9f68c1f
Jian Li <monadli@gmail.com>
2020-09-26 23:54:38 -0400

    changed ftl_unit_test to emit more info at failure

--- src/ts/test/ftl_unit_tests.ts
+++ src/ts/test/ftl_unit_tests.ts
@@ -9,28 +9,32 @@ ftl_builder.setRunPath(run_path)
 
 let ftl_files = fs.readdirSync(ftl_test_path)
 for (let file of ftl_files) {
-    if (file.endsWith('.ftl')) {
-        let module:ftl.Module = ftl_builder.buildModule(run_path, `/src/ftl/test/${file.substring(0, file.length - 4)}`)
+  if (file.endsWith('.ftl')) {
+    try {
+      let module:ftl.Module = ftl_builder.buildModule(run_path, `/src/ftl/test/${file.substring(0, file.length - 4)}`)
 
-        console.log(`${file} (${module.executables.length})`)
-        var passed = 0
-        module.executables.forEach(e => {
-            try {
-              let res = e.apply()
-              if (res !== undefined) {
-                if (Array.isArray(res) || typeof res == 'string') {
-                  console.log(JSON.stringify(res));
-                } else if (res === true) {
-                  passed++
-                  console.log(res);
-                } else {
-                  console.log(res);
-                }
-              }
-            } catch (e) {
-              console.error(e)
+      console.log(`${file} (${module.executables.length})`)
+      var passed = 0
+      module.executables.forEach(e => {
+        try {
+          let res = e.apply()
+          if (res !== undefined) {
+            if (Array.isArray(res) || typeof res == 'string') {
+              console.log(JSON.stringify(res));
+            } else if (res === true) {
+              passed++
+              console.log(res);
+            } else {
+              console.log(res);
             }
-        })
-        console.log(passed == module.executables.length ? 'PASSED!\n' : 'FAILED!\n')
+          }
+        } catch (e) {
+          console.error(e)
+        }
+      })
+      console.log(passed == module.executables.length ? 'PASSED!\n' : 'FAILED!\n')
+    } catch (e) {
+      console.error(`\nFailed testing ${file} with error ${e}!\n`)
     }
+  }
 }
